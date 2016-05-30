4bb01158e1842a127e1c0397e58af74e16328232
Tim MacDonald <Tim MacDonald>
2016-05-30 15:58:56 -0700

    Closer E190 values

--- scripts/experimental/geometry/E190.py
+++ scripts/experimental/geometry/E190.py
@@ -226,7 +226,7 @@ def vehicle_setup():
     wing.twists.root             = 2.0 * Units.degrees
     wing.twists.tip              = 0.0 * Units.degrees
 
-    wing.origin                  = [20,0,0]
+    wing.origin                  = [14.5,0,0]
     wing.aerodynamic_center      = [3,0,0]
 
     wing.tip_location            = find_tip_chord_leading_edge(wing)
@@ -266,7 +266,7 @@ def vehicle_setup():
     wing.twists.root             = 2.0 * Units.degrees
     wing.twists.tip              = 2.0 * Units.degrees
 
-    wing.origin                  = [35,0,0]
+    wing.origin                  = [33,0,0]
     wing.aerodynamic_center      = [2,0,0]
     
     wing.tip_location            = find_tip_chord_leading_edge(wing)
@@ -308,7 +308,7 @@ def vehicle_setup():
     
     wing.tip_location            = find_tip_chord_leading_edge(wing)
 
-    wing.origin                  = [35,0,0]
+    wing.origin                  = [30,0,0]
     wing.aerodynamic_center      = [2,0,0]
 
     wing.vertical                = True
