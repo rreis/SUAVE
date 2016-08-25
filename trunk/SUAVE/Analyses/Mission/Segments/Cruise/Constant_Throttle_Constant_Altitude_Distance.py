c70cbfe870623e73faa88374b6516135b9aa8b91
Tim MacDonald <timdmacdo@gmail.com>
2016-08-24 17:10:58 -0700

    first attempt

---
+++ trunk/SUAVE/Analyses/Mission/Segments/Cruise/Constant_Throttle_Constant_Altitude_Distance.py
@@ -0,0 +1,121 @@
+# Constant_Throttle_Constant_Altitude.py
+#
+# Created:  
+# Modified: Feb 2016, Andrew Wendorff
+
+# ----------------------------------------------------------------------
+#  Imports
+# ----------------------------------------------------------------------
+
+# SUAVE imports
+from SUAVE.Analyses.Mission.Segments import Aerodynamic
+from SUAVE.Analyses.Mission.Segments import Conditions
+
+from SUAVE.Methods.Missions import Segments as Methods
+
+from SUAVE.Analyses import Process
+
+# ----------------------------------------------------------------------
+#  Segment
+# ----------------------------------------------------------------------
+
+class Constant_Throttle_Constant_Altitude_Distance(Aerodynamic):
+    
+    # ------------------------------------------------------------------
+    #   Data Defaults
+    # ------------------------------------------------------------------  
+
+    def __defaults__(self):
+        
+        # --------------------------------------------------------------
+        #   User inputs
+        # --------------------------------------------------------------
+        self.throttle        = None
+        self.altitude        = None
+        self.distance        = 10000.
+        
+        # --------------------------------------------------------------
+        #   State
+        # --------------------------------------------------------------
+    
+        # conditions
+        self.state.conditions.update( Conditions.Aerodynamics() )
+    
+        # initials and unknowns
+        ones_row = self.state.ones_row
+        self.state.unknowns.body_angle            = ones_row(1) * 0.0
+        self.state.unknowns.velocity_x            = ones_row(1) * 200.0
+        self.state.unknowns.position_x            = ones_row(1) * 1.0
+        self.state.unknowns.time                  = 0.1
+        self.state.residuals.final_distance_error = 0.0
+        self.state.residuals.distance_error       = ones_row(1) * 0.0
+        self.state.residuals.forces               = ones_row(2) * 0.0
+    
+        # --------------------------------------------------------------
+        #   The Solving Process
+        # --------------------------------------------------------------
+    
+        # --------------------------------------------------------------
+        #   Initialize - before iteration
+        # --------------------------------------------------------------
+        initialize = self.process.initialize
+    
+        initialize.expand_state            = Methods.expand_state
+        initialize.differentials           = Methods.Common.Numerics.initialize_differentials_dimensionless
+        initialize.conditions              = Methods.Cruise.Constant_Throttle_Constant_Altitude_Distance.initialize_conditions        
+    
+        # --------------------------------------------------------------
+        #   Converge - starts iteration
+        # --------------------------------------------------------------
+        converge = self.process.converge
+    
+        converge.converge_root             = Methods.converge_root    
+       
+        # --------------------------------------------------------------
+        #   Iterate - this is iterated
+        # --------------------------------------------------------------
+        iterate = self.process.iterate
+                
+        # Update Initials
+        iterate.initials = Process()
+        iterate.initials.time              = Methods.Common.Frames.initialize_time
+        iterate.initials.weights           = Methods.Common.Weights.initialize_weights
+        iterate.initials.inertial_position = Methods.Common.Frames.initialize_inertial_position
+        iterate.initials.planet_position   = Methods.Common.Frames.initialize_planet_position
+        
+        # Unpack Unknowns
+        iterate.unknowns = Process()
+        iterate.unknowns.mission           = Methods.Cruise.Constant_Throttle_Constant_Altitude_Distance.unpack_unknowns        
+    
+        # Update Conditions
+        iterate.conditions = Process()
+        iterate.conditions.differentials   = Methods.Common.Numerics.update_differentials_time
+        iterate.conditions.altitude        = Methods.Common.Aerodynamics.update_altitude
+        iterate.conditions.atmosphere      = Methods.Common.Aerodynamics.update_atmosphere
+        iterate.conditions.gravity         = Methods.Common.Weights.update_gravity
+        iterate.conditions.freestream      = Methods.Common.Aerodynamics.update_freestream
+        iterate.conditions.orientations    = Methods.Common.Frames.update_orientations
+        iterate.conditions.aerodynamics    = Methods.Common.Aerodynamics.update_aerodynamics
+        iterate.conditions.stability       = Methods.Common.Aerodynamics.update_stability
+        iterate.conditions.propulsion      = Methods.Common.Energy.update_thrust
+        iterate.conditions.weights         = Methods.Common.Weights.update_weights
+        iterate.conditions.forces          = Methods.Common.Frames.update_forces
+        iterate.conditions.planet_position = Methods.Common.Frames.update_planet_position
+    
+        
+        # Solve Residuals
+        iterate.residuals = Process()     
+        iterate.residuals.total_forces     = Methods.Cruise.Constant_Throttle_Constant_Altitude_Distance.solve_residuals
+    
+        # --------------------------------------------------------------
+        #   Finalize - after iteration
+        # --------------------------------------------------------------
+        finalize = self.process.finalize
+    
+        # Post Processing
+        finalize.post_process = Process()        
+        finalize.post_process.inertial_position = Methods.Common.Frames.integrate_inertial_horizontal_position
+        #finalize.post_process.stability         = Methods.Common.Aerodynamics.update_stability  
+        finalize.post_process.cruise            = Methods.Cruise.Constant_Throttle_Constant_Altitude_Distance.post_process
+
+        return
