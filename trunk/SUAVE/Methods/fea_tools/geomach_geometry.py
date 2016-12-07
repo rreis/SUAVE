60a370d84d53090a2386441863623f8f5587f8da
jmvegh <jmvegh@stanford.edu>
2016-12-07 10:44:34 -0800

    try something else (root section was okay before this)

--- trunk/SUAVE/Methods/fea_tools/geomach_geometry.py
+++ trunk/SUAVE/Methods/fea_tools/geomach_geometry.py
@@ -26,9 +26,9 @@ def geometry_generation(aircraft,geomach_structural_mesh,structural_surface_grid
         pgm.dvs['lwing_section_1_z'].data[0] = aircraft.main_wing[0].main_wing_section[0].root_origin[2] #2.6
         print 'aircraft.main_wing[0].main_wing_section[0].root_origin=', aircraft.main_wing[0].main_wing_section[0].root_origin
         #relative to the root
-        pgm.dvs['lwing_section_2_x'].data[0] = aircraft.main_wing[0].main_wing_section[0].tip_origin[0]-aircraft.main_wing[0].main_wing_section[0].root_origin[0] #16.5
-        pgm.dvs['lwing_section_2_y'].data[0] = aircraft.main_wing[0].main_wing_section[0].tip_origin[1]-aircraft.main_wing[0].main_wing_section[0].root_origin[1] #4.4
-        pgm.dvs['lwing_section_2_z'].data[0] = aircraft.main_wing[0].main_wing_section[0].tip_origin[2]-aircraft.main_wing[0].main_wing_section[0].root_origin[2]  #23.3
+        pgm.dvs['lwing_section_2_x'].data[0] = aircraft.main_wing[0].main_wing_section[0].tip_origin[0]#-aircraft.main_wing[0].main_wing_section[0].root_origin[0] #16.5
+        pgm.dvs['lwing_section_2_y'].data[0] = aircraft.main_wing[0].main_wing_section[0].tip_origin[1]#-aircraft.main_wing[0].main_wing_section[0].root_origin[1] #4.4
+        pgm.dvs['lwing_section_2_z'].data[0] = aircraft.main_wing[0].main_wing_section[0].tip_origin[2]#-aircraft.main_wing[0].main_wing_section[0].root_origin[2]  #23.3
         print 'aircraft.main_wing[0].main_wing_section[0].tip_origin=', aircraft.main_wing[0].main_wing_section[0].root_origin                                                                                               #
         pgm.dvs['lwing_section_3_x'].data[0] = aircraft.main_wing[0].main_wing_section[1].tip_origin[0]#-aircraft.main_wing[0].main_wing_section[1].root_origin[0] #16.5
         pgm.dvs['lwing_section_3_y'].data[0] = aircraft.main_wing[0].main_wing_section[1].tip_origin[1]#-aircraft.main_wing[0].main_wing_section[1].root_origin[1] #4.4
@@ -36,9 +36,9 @@ def geometry_generation(aircraft,geomach_structural_mesh,structural_surface_grid
                                                                                                        #
         #print aircraft.main_wing[0].main_wing_section[2].tip_origin[0]                                #
         #print aircraft.main_wing[2].main_wing_section[2]                                              #
-        pgm.dvs['lwing_section_4_x'].data[0] = aircraft.main_wing[0].main_wing_section[2].tip_origin[0]-aircraft.main_wing[0].main_wing_section[2].root_origin[0] #16.5
-        pgm.dvs['lwing_section_4_y'].data[0] = aircraft.main_wing[0].main_wing_section[2].tip_origin[1]-aircraft.main_wing[0].main_wing_section[2].root_origin[1] #4.4
-        pgm.dvs['lwing_section_4_z'].data[0] = aircraft.main_wing[0].main_wing_section[2].tip_origin[2]-aircraft.main_wing[0].main_wing_section[2].root_origin[2]  #23.3
+        pgm.dvs['lwing_section_4_x'].data[0] = aircraft.main_wing[0].main_wing_section[2].tip_origin[0]#-aircraft.main_wing[0].main_wing_section[2].root_origin[0] #16.5
+        pgm.dvs['lwing_section_4_y'].data[0] = aircraft.main_wing[0].main_wing_section[2].tip_origin[1]#-aircraft.main_wing[0].main_wing_section[2].root_origin[1] #4.4
+        pgm.dvs['lwing_section_4_z'].data[0] = aircraft.main_wing[0].main_wing_section[2].tip_origin[2]#-aircraft.main_wing[0].main_wing_section[2].root_origin[2]  #23.3
         
         
         
