33316e73c63ed0c1a14e68e16636445c92eb3204
jmvegh <jmvegh@stanford.edu>
2016-12-07 11:32:27 -0800

    took out print statements, trying copy

--- scripts/experimental/geometry/B737.py
+++ scripts/experimental/geometry/B737.py
@@ -512,7 +512,7 @@ def vehicle_setup():
   
     wing_section[1].type =  'wing_section'
     wing_section[1].root_chord = wing_section[0].tip_chord
-    wing_section[1].tip_chord   = wing.chords.tip
+    wing_section[1].tip_chord   = wing.chords.tip*1.
     wing_section[1].root_origin = wing_section[0].tip_origin*1. 
     wing_section[1].span        = (1-.91)*wing.spans.projected*.5
     wing_section[1].sweep       = 48.17*Units.degrees
