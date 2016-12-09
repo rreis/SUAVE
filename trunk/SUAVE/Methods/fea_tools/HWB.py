4b4850cbeab8009f0a6a77412c8b74530f68c13c
jmvegh <jmvegh@stanford.edu>
2016-12-09 08:12:39 -0800

    try it for running HWB.py w/ 40 planes

--- trunk/SUAVE/Methods/fea_tools/HWB.py
+++ trunk/SUAVE/Methods/fea_tools/HWB.py
@@ -16,7 +16,7 @@ class HWB(PGMconfiguration):
     
     def _define_comps(self):
         self.nplanes = 40
-        self.num_sections = 7
+        self.num_sections = 3
         nplanes = self.nplanes
         self.comps['lwing'] = PGMwing(num_x=nplanes , num_z=nplanes , left_closed=True)
         self.comps['lwing_t'] = PGMtip(self, 'lwing', 'left', 0.1)
